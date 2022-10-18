7df47eb9a2368ebf3a8beb1216e5d70800978b9a
Diego <da_sandoval@hotmail.com>
2022-10-18 10:58:19 -0300

    homework check

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -259,7 +259,7 @@ function esVocal(letra){
   //Escribe tu código aquí
   /*
   if(letra.length>1){
-    return "Dato incorrecto" ;
+    return "Dato incorrecto";
     }
  if (letra === 'a'|| letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u')
   {
